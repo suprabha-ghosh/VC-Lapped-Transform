71bf2bed6d006cb73d90e7bc14af18c895e0fd50
Suprabha Ghosh <suprabhaghosh5@gmail.com>
2024-07-31 22:56:18 +0200

    Add files via upload

---
+++ path.py
@@ -0,0 +1,7 @@
+# Define universal paths here
+PATH_COLOR = '/home/jupyter-64365/VC_Seminar_Project/color'
+PATH_GRAY = '/home/jupyter-64365/VC_Seminar_Project/gray'
+PATH_COLOR_DCT = '/home/jupyter-64365/VC_Seminar_Project/color-dct'
+PATH_GRAY_DCT = '/home/jupyter-64365/VC_Seminar_Project/gray-dct'
+PATH_COLOR_LAP = '/home/jupyter-64365/VC_Seminar_Project/color-lapped'
+PATH_GRAY_LAP = '/home/jupyter-64365/VC_Seminar_Project/gray-lapped'
