b734444826018d27731574fda214411982ec2e68
Suprabha Ghosh <suprabhaghosh5@gmail.com>
2024-08-27 14:01:19 +0200

    Add files via upload

---
+++ path.py
@@ -0,0 +1,15 @@
+# Define universal paths here
+PATH_COLOR = '/home/jupyter-64365/VC_Seminar_Project/color'
+PATH_GRAY = '/home/jupyter-64365/VC_Seminar_Project/gray'
+PATH_COLOR_DCT = '/home/jupyter-64365/VC_Seminar_Project/color-dct'
+PATH_GRAY_DCT = '/home/jupyter-64365/VC_Seminar_Project/gray-dct'
+PATH_COLOR_LAP = '/home/jupyter-64365/VC_Seminar_Project/color-lapped'
+PATH_GRAY_LAP = '/home/jupyter-64365/VC_Seminar_Project/gray-lapped'
+
+# CSV output paths
+PATH_COLOR_RATIO = '/home/jupyter-64365/VC_Seminar_Project/color_com_ratio.csv'
+PATH_GRAY_RATIO = '/home/jupyter-64365/VC_Seminar_Project/gray_com_ratio.csv'
+PATH_COLOR_LPIPS_DCT = '/home/jupyter-64365/VC_Seminar_Project/color_lpips_results_dct.csv'
+PATH_COLOR_LPIPS_LAPPED = '/home/jupyter-64365/VC_Seminar_Project/color_lpips_results_Lapped.csv'
+PATH_GRAY_LPIPS_DCT = '/home/jupyter-64365/VC_Seminar_Project/gray_lpips_results_dct.csv'
+PATH_GRAY_LPIPS_LAPPED = '/home/jupyter-64365/VC_Seminar_Project/gray_lpips_results_Lapped.csv'
